ba169a7785d7ed9ea677b7859cf7fe1e7108a4be
Kate Trahan and Paul Kotok <paul@>
2017-11-27 16:54:46 -0800

    new things

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
-var mySentence = prompt("write a sentence.");
+var mySentence = prompt("write a sentence."); //comes back from prompt input: Toast Output TT
 
-var first= mySentence.charAt(0);
-var last= mySentence.charAt(mySentence.length-1);
+var first = mySentence.charAt(0); //first char, T
+var last = mySentence.charAt(mySentence.length-1); //0 based index, 5 -1 =4, T
 
 
-var newString = function(first,last){
- return first.toUpperCase() + last.toUpperCase()}
+function newString(){
+  var someVariable = "hello";
+ return first.toUpperCase() + last.toUpperCase()
+}
 
-alert (newString(first, last));
+alert (newString());
+
+function reverseString (){
+  var firstAndLast = newString();
+  return firstAndLast.charAt(1) + firstAndLast.charAt(0);
+}
+
+alert (reverseString());
+
+
+alert (newString();
+
+// alert (mySentence + newString);
+
+
+
+
+
+
+//
+// var twoStrings = function (mySentence, newString){
+//   return reverseString + mySentence;
+// };
+//
+// alert (twoStrings)
+ //       -------
+
+ // var twostrings = function(first,last){
+ //   return last.toUpperCase() + first.toUpperCase()
+ // }
+ //
+ // alert (twoStrings(first, last));
+// mysentence + reverseString
